af49bf402dceeed84aac9e2fdc230fc089db4a4c
Victor Fernandes <victorpet@gmail.com>
2018-02-19 12:58:47 -0400

    commit problem inserted

--- app/src/main/java/br/com/victorpettengill/hawk_eyedcitizen/ui/FindLocationActivity.java
+++ app/src/main/java/br/com/victorpettengill/hawk_eyedcitizen/ui/FindLocationActivity.java
@@ -2,11 +2,17 @@ package br.com.victorpettengill.hawk_eyedcitizen.ui;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -15,6 +21,7 @@ import br.com.victorpettengill.hawk_eyedcitizen.R;
 public class FindLocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private final String TAG = "FindLocation";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,30 @@ public class FindLocationActivity extends FragmentActivity implements OnMapReady
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: obter informações sobre o local selecionado.
+                Log.i(TAG, "Place: " + place.getName());
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        place.getLatLng(),
+                        13));
+                mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getAddress().toString()));
+
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Solucionar o erro.
+                Log.i(TAG, "Ocorreu um erro: " + status);
+            }
+        });
+
     }
 
 
@@ -40,9 +71,5 @@ public class FindLocationActivity extends FragmentActivity implements OnMapReady
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
